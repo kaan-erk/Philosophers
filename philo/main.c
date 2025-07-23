612a6e3299cee769a04754eeaa9e2626632c5913
Kaan Erk Toraman <ktoraman@k1m23s06.42istanbul.com.tr>
2025-07-23 17:15:02 +0300

    Parse Error Debug

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: ktoraman < ktoraman@student.42istanbul.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 13:20:26 by ktoraman          #+#    #+#             */
-/*   Updated: 2025/07/23 16:57:40 by ktoraman         ###   ########.fr       */
+/*   Updated: 2025/07/23 17:08:49 by ktoraman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int main(int ac, char **av)
 {
 	(void)av;
-	if (ac == 5)
+	if (ac == 5 || ac == 6)
 	{
 		if (parse(av))
 			return (1);
--- philo/parse/parse.c
+++ philo/parse/parse.c
@@ -6,7 +6,7 @@
 /*   By: ktoraman < ktoraman@student.42istanbul.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 16:56:07 by ktoraman          #+#    #+#             */
-/*   Updated: 2025/07/23 16:56:30 by ktoraman         ###   ########.fr       */
+/*   Updated: 2025/07/23 17:14:15 by ktoraman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,25 +36,18 @@ int parse_table_error(char **av)
 {
 	if (alpha_check(av))
 		return(ft_putendl_fd("Alphabetical Character in args", 2) ,1);
-	if (!(ft_atoll(av[1]) < INT_MAX && ft_atoll(av[1]) > -1))
-	{
-		ft_putendl_fd("Invalid integer for Number of philosophers", 2);
-		return (1);
-	}
+	if (!(ft_atoll(av[1]) < INT_MAX && ft_atoll(av[1]) > 0))
+			return (ft_putendl_fd("Invalid integer for number of philosophers", 2), 1);
 	if (!(ft_atoll(av[2]) < INT_MAX && ft_atoll(av[2]) > -1))
-	{
-		ft_putendl_fd("Invalid integer for time to die", 2);
-		return (1);
-	}
+			return (ft_putendl_fd("Invalid integer for time to die", 2), 1);
 	if (!(ft_atoll(av[3]) < INT_MAX && ft_atoll(av[3]) > -1))
-	{
-		ft_putendl_fd("Invalid integer for time to eat", 2);
-		return (1);
-	}
+			return (ft_putendl_fd("Invalid integer for time to eat", 2), 1);
 	if (!(ft_atoll(av[4]) < INT_MAX && ft_atoll(av[4]) > -1))
+		return (ft_putendl_fd("Invalid integer for time to sleep", 2), 1);
+	if (av[5])
 	{
-		ft_putendl_fd("Invalid integer for time to sleep", 2);
-		return (1);
+		if (!(ft_atoll(av[5]) < INT_MAX && ft_atoll(av[5]) > -1))	
+			return (ft_putendl_fd("Invalid integer for meal goal", 2), 1);
 	}
 	return (0);
 }
